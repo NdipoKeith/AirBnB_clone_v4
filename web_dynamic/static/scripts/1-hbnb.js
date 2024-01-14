87790b9b67340f6d572c9285c7c8bfc534e45fbc
DAVENJAGI <njagidave39@gmail.com>
2024-01-14 23:39:22 +0300

    Dynamic_web_semistandard_compliant

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-	let checkedAmenities = {};
-	$(document).on('change', "input[type='checkbox']", fucntion() {
-		if (this.checked) {
-			checkedAmenities[$(this).data('id')] = $(this).data('name');
-		} else {
-			delete checkedAmenities[$(this).data('id')];
-		}
-		let list = object.calues(checkedAmenities);
-		if (list.length > 0) {
-			$('div.amenities > h4').text(object.values(checkedAmenities).json(', '));
-		} else {
-			$('div.amenities > h4').html('&nbsp;');
-		}
-	});
+$(document).ready(function () {
+  const checkedAmenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checkedAmenities[$(this).data('id')];
+    }
+    const list = Object.values(checkedAmenities);
+    if (list.length > 0) {
+      $('div.amenities > h4').text(Object.values(checkedAmenities).json(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
 });
